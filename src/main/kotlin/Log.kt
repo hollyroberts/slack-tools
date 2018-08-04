532a5c86f94524f69f2bd3e861d3049d5a9ba763
blackenedgem <Chrisroberts97@btinternet.com>
2018-08-04 16:23:27 +0100

    Basic logging implemenation

---
+++ src/main/kotlin/Log.kt
@@ -0,0 +1,15 @@
+object Log {
+    enum class Modes { DEBUG, INFO, WARN, ERROR }
+    val mode = Modes.INFO
+
+    private fun log(mode: Modes, message: String) {
+        if (mode >= this.mode) {
+            println("[${mode.name}] - $message")
+        }
+    }
+
+    fun debug(message: String) = log(Modes.DEBUG, message)
+    fun info(message: String) = log(Modes.INFO, message)
+    fun warn(message: String) = log(Modes.WARN, message)
+    fun error(message: String) = log(Modes.ERROR, message)
+}
