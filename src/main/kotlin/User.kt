7ff0e667c19f54ae5400b58040dc52fea3078863
blackenedgem <Chrisroberts97@btinternet.com>
2018-08-04 14:04:10 +0100

    Manually parse profile, provide method to get url of largest image

--- src/main/kotlin/User.kt
+++ src/main/kotlin/User.kt
@@ -1,4 +1,4 @@
-import com.beust.klaxon.Klaxon
+import com.beust.klaxon.*
 
 data class User(
         // Strings and profile
@@ -11,14 +11,63 @@ data class User(
         val is_bot: Boolean
 )
 
+object ProfileConverter: Converter {
+    // Don't support converting from object to JSON
+    override fun toJson(value: Any): String {
+        return "Not implemented"
+    }
+
+    override fun canConvert(cls: Class<*>) = cls == Profile::class.java
+    override fun fromJson(jv: JsonValue): Profile {
+        // Get obj, then display name string from object
+        val obj = jv.obj ?: throw KlaxonException("Null profile")
+        val display_name = obj["display_name"] as? String ?: throw KlaxonException("No display_name in profile")
+
+
+        // Get the map of images
+        val images = mutableMapOf<String, String>()
+        obj.filterKeys { it.startsWith("image_") }
+                .forEach { key, value ->
+                    images[key.removePrefix("image_")] =
+                            value as? String ?: throw KlaxonException("$key did contain a string") }
+        if (images.isEmpty()) {
+            throw KlaxonException("No images found for profile")
+        }
+
+        return Profile(display_name, images)
+    }
+}
+
 data class Profile(
-        val display_name: String
-)
+        val display_name: String,
+        val images: Map<String, String>
+) {
+
+    /**
+     * Returns the URL of the largest avatar image in the profile
+     */
+    fun getLargestImage() : String {
+        // Try to get the original
+        if (images.containsKey("original")) {
+            return images.getValue("original")
+        }
+
+        // Get the largest number
+        return images
+                .filterKeys { when(it.toIntOrNull()){
+                    null -> false
+                    else -> true
+                }}
+                .maxBy { it.key.toInt() }!!.value
+    }
+}
 
 fun main(args: Array<String>) {
     val f = User::class.java.getResource("/test.json")
     val t = f.readText()
-    print(t)
-    val u = Klaxon().parse<User>(t)
-    print(u)
+    println(t)
+    val u = Klaxon()
+            .converter(ProfileConverter)
+            .parse<User>(t)!!
+    println(u.profile.getLargestImage())
 }
