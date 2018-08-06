bd19abc50aafeedb40e3e031641bcc5b98315787
blackenedgem <Chrisroberts97@btinternet.com>
2018-08-06 17:41:21 +0100

    Format size

--- src/main/kotlin/File.kt
+++ src/main/kotlin/File.kt
@@ -1,3 +1,8 @@
+import java.text.DecimalFormat
+import kotlin.math.log
+import kotlin.math.max
+import kotlin.math.pow
+
 data class File(
         // Identication
         val id: String,
@@ -13,4 +18,18 @@ data class File(
         // Won't be included if file object is directly from a channel
         val channels: List<String>?,
         val ims: List<String>
-)
+) {
+    /**
+     * Formats the size of the file into a human readable version
+     * @param precision Number of decimal places to return (there will be at least 1)
+     */
+    fun formattedSize(precision: Int) : String {
+        if (size < 1024) return "$size B"
+        val exp = log(size.toDouble(), 1024.0).toInt()
+        val prefix = "KMGTPE"[exp - 1]
+
+        val df = DecimalFormat("#.0" + "#".repeat(max(0, precision - 1)))
+        val formattedSize = df.format(size / (1024.0).pow(exp))
+        return "$formattedSize ${prefix}iB"
+    }
+}
