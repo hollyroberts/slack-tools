d9f3ab7318f2c886ad8fee55b634ad0a84e1c4f0
blackenedgem <Chrisroberts97@btinternet.com>
2018-09-03 21:36:09 +0200

    Update build script for kotlin changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,42 +1,30 @@
-@file:Suppress("PropertyName")
-
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-buildscript {
-    var kotlin_version: String by extra
-    kotlin_version = "1.2.50"
-
-    repositories {
-        mavenCentral()
-    }
-    dependencies {
-        classpath(kotlinModule("gradle-plugin", kotlin_version))
-    }
+plugins {
+    java
+    kotlin("jvm") version "1.2.61"
 }
 
 version = "0.1-DEV"
 
-apply {
-    plugin("kotlin")
-    plugin("kotlin-kapt")
-}
-
-val kotlin_version: String by extra
-
 repositories {
     mavenCentral()
-    jcenter()
 }
 
 dependencies {
-    compile(kotlinModule("stdlib-jdk8", kotlin_version))
+    compile(kotlin("stdlib-jdk8"))
     compile("com.squareup.okhttp3:okhttp:3.11.0")
     compile("com.squareup.moshi:moshi:1.6.0")
     compile("com.github.ajalt:clikt:1.4.0")
 
     kapt("com.squareup.moshi:moshi-kotlin-codegen:1.6.0")
+
+    testCompile("junit", "junit", "4.12")
 }
 
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
