70c883a84eaae5c66470399ad069de7e2112f315
blackenedgem <Chrisroberts97@btinternet.com>
2018-09-04 17:01:28 +0200

    Convert uploadLoc from var to val

--- src/main/kotlin/slackjson/CompleteFile.kt
+++ src/main/kotlin/slackjson/CompleteFile.kt
@@ -4,12 +4,13 @@ import utils.Api
 import utils.Log
 
 class CompleteFile(pf: ParsedFile) : ParsedFile(pf) {
-    var uploadLoc: String? = null
-
     init {
-        when {
-            channelsUploadedIn() == 1 -> uploadLoc = channels?.firstOrNull() ?: groups?.firstOrNull() ?: ims!![0]
-            channelsUploadedIn() == 0 -> Log.info("File $id belongs to no channels")
+        val uploadLoc = when {
+            channelsUploadedIn() == 1 -> channels?.firstOrNull() ?: groups?.firstOrNull() ?: ims!![0]
+            channelsUploadedIn() == 0 -> {
+                Log.info("File $id belongs to no channels")
+                null
+            }
             else -> {
                 Log.debugHigh("File $id belongs to more than one channel, requires API call to resolve")
                 resolveMultipleLocations(this)
@@ -21,14 +22,17 @@ class CompleteFile(pf: ParsedFile) : ParsedFile(pf) {
     /**
      * Parsed files can have multiple channels, we want to figure out where it was shared to first
      * Used during initialisation
+     * Returns id of channel that file was first seen in
      */
-    private fun resolveMultipleLocations(cf: CompleteFile) {
+    private fun resolveMultipleLocations(cf: CompleteFile) : String? {
         val f = Api.getFile(cf.id)
 
         with(f.firstSeen!!) {
             if (this.isNotEmpty()) {
-                cf.uploadLoc = this.minBy { it.value }!!.key
+                return this.minBy { it.value }!!.key
             }
         }
+
+        return null
     }
 }
