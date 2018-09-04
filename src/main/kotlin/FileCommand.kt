f0c9137923e771706d0acf8ee1352a1ef26184bd
blackenedgem <Chrisroberts97@btinternet.com>
2018-09-04 23:22:40 +0200

    Add nice output messages when locating files

--- src/main/kotlin/FileCommand.kt
+++ src/main/kotlin/FileCommand.kt
@@ -8,11 +8,32 @@ object FileCommand: CliktCommand(
         name = "file",
         help = "Downloads files from slack") {
 
+    private const val LOCATION_INTERVAL = 3000
+
     override fun run() {
         val filesRaw: MutableList<SlackFile> = Api.getFiles().toMutableList()
 
-        Log.info("Locating upload location of files (this may take a while, especially if inference is disabled)")
         // Start timer to output process every X seconds
-        filesRaw.replaceAll { CompleteFile(it) }
+        Log.info("Locating upload location of files (this may take a while, especially if inference is disabled)")
+        val startTime = System.currentTimeMillis()
+        var nextOutputTime = startTime + LOCATION_INTERVAL
+
+        // Iterate over objects, and replace with complete version
+        for ((index, obj) in filesRaw.withIndex()) {
+            filesRaw[index] = CompleteFile(obj)
+
+            if (System.currentTimeMillis() > nextOutputTime) {
+                Log.info("Processed ${index + 1}/${filesRaw.size} files")
+                nextOutputTime = System.currentTimeMillis() + LOCATION_INTERVAL
+            }
+        }
+
+        // Output timed messages if took more than LOCATION_INTERVAL
+        val timeTaken = System.currentTimeMillis() - startTime
+        if (timeTaken > LOCATION_INTERVAL) {
+            Log.info(String.format("Located the upload location of all files in %.1f seconds", timeTaken.toFloat() / 1000))
+        } else {
+            Log.info("Files located")
+        }
     }
 }
