b9ca94d769c618287cad1c3817eff9e68c24c211
blackenedgem <Chrisroberts97@btinternet.com>
2018-09-04 11:30:08 +0200

    Refactor completefile away from factory ish pattern, use initialisation block instead

--- src/main/kotlin/slackjson/CompleteFile.kt
+++ src/main/kotlin/slackjson/CompleteFile.kt
@@ -3,41 +3,28 @@ package slackjson
 import utils.Api
 import utils.Log
 
-class CompleteFile private constructor() : ParsedFile() {
+class CompleteFile private constructor(pf: ParsedFile) : ParsedFile(pf) {
     var uploadLoc: String? = null
 
-    companion object {
-        /**
-         * Take a parsed file and perform the appropriate actions to make it whole
-         */
-        fun create(pf: ParsedFile) : CompleteFile {
-            // Cast from parsed file to complete file to keep properties
-            val cf = pf as CompleteFile
-
-            with(cf) {
-                // Note: If channels uploaded in is 0, then we don't know where the file was uploaded to
-                // This can happen if the file is uploaded, and then the file message is deleted
-
-                when {
-                    channelsUploadedIn() == 1 -> uploadLoc = channels?.firstOrNull() ?: groups?.firstOrNull() ?: ims!![0]
-                    channelsUploadedIn() == 0 -> Log.debugHigh("File ${cf.id} has no channels")
-                    else -> resolveMultipleLocations(this)
-                }
-            }
-
-            return cf
+    init {
+        when {
+            channelsUploadedIn() == 1 -> uploadLoc = channels?.firstOrNull() ?: groups?.firstOrNull() ?: ims!![0]
+            channelsUploadedIn() == 0 -> Log.debugHigh("File $id has no channels")
+            else -> resolveMultipleLocations(this)
         }
+    }
 
-        /**
-         * Parsed files can have multiple channels, we want to figure out where it was shared to first
-         */
-        private fun resolveMultipleLocations(cf: CompleteFile) {
-            val f = Api.getFile(cf.id)
 
-            with(f.firstSeen!!) {
-                if (this.isNotEmpty()) {
-                    cf.uploadLoc = this.minBy { it.value }!!.key
-                }
+    /**
+     * Parsed files can have multiple channels, we want to figure out where it was shared to first
+     * Used during initialisation
+     */
+    private fun resolveMultipleLocations(cf: CompleteFile) {
+        val f = Api.getFile(cf.id)
+
+        with(f.firstSeen!!) {
+            if (this.isNotEmpty()) {
+                cf.uploadLoc = this.minBy { it.value }!!.key
             }
         }
     }
