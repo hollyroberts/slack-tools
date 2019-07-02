5d55bae9330d88cf46a57857c96860ab282ada0e
blackenedgem <Chrisroberts97@btinternet.com>
2019-07-02 21:16:42 +0100

    Add additional time zone options

--- src/main/kotlin/scripts/TimeOptions.kt
+++ src/main/kotlin/scripts/TimeOptions.kt
@@ -1,11 +1,15 @@
 package scripts
 
 import com.github.ajalt.clikt.parameters.groups.OptionGroup
+import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
+import utils.Log
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
+import java.util.*
 
 /**
  * Contains internally the string representations of parsed options
@@ -18,10 +22,21 @@ class TimeOptions : OptionGroup(
 ) {
     data class Options(
             val startTime: ZonedDateTime?,
-            val endTime: ZonedDateTime?
+            val endTime: ZonedDateTime?,
+            val outTz: ZoneId
     )
 
     private val dtf = DateTimeFormatter.ofPattern("dd/MM/yy")
+
+    // Date input options
+    private val printTzs by option("--timezone-list", "-tzl",
+            help = "Prints out the available timezones and the current timezone").flag()
+    private val inputTz by option("--timezone-input", "-tzi",
+            help = "The timezone for the datetimes given via arguments")
+    private val outputTz by option("--timezone-output", "-tzo",
+            help = "The timezone for any information output (eg. message/file times)")
+
+    // Start/end time
     private val startTimeStr by option("--start-time", "-ts",
             help = "Include anything after this time (inclusive)",
             metavar = "DATETIME")
@@ -33,8 +48,26 @@ class TimeOptions : OptionGroup(
      * Return the data class of parsed options
      */
     fun options(): Options {
-        val timeZone = ZoneId.systemDefault()
+        // Zone information
+        if (printTzs) {
+            Log.high("Available timezones: ")
+            Log.high(ZoneId.getAvailableZoneIds().sorted().joinToString("\n       "))
+            Log.high("Current timezone: " + ZoneId.systemDefault())
+        }
 
+        // Input/output zone
+        val timeZone = if (inputTz != null) {
+            ZoneId.of(inputTz)
+        } else {
+            ZoneId.systemDefault()
+        }
+        val outputTimeZone = if (outputTz != null) {
+            ZoneId.of(outputTz)
+        } else {
+            ZoneId.systemDefault()
+        }
+
+        // Parse datetime
         val startTime = startTimeStr?.let {
             val startTimeLocal = LocalDateTime.parse(it, dtf)
             ZonedDateTime.of(startTimeLocal, timeZone)
@@ -44,6 +77,6 @@ class TimeOptions : OptionGroup(
             ZonedDateTime.of(endTimeLocal, timeZone)
         }
 
-        return Options(startTime, endTime)
+        return Options(startTime, endTime, outputTimeZone)
     }
 }
