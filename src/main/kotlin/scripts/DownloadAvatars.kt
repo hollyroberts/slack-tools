7a6a9028ec03730a1c65093c5eaf6729b1eaba04
blackenedgem <Chrisroberts97@btinternet.com>
2019-07-03 21:57:28 +0100

    Add option to include date. Also improve handling of displaynames

--- src/main/kotlin/scripts/DownloadAvatars.kt
+++ src/main/kotlin/scripts/DownloadAvatars.kt
@@ -7,8 +7,11 @@ import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.file
+import slackjson.User
 import utils.*
 import java.io.File
+import java.time.Instant
+import java.time.format.DateTimeFormatter
 
 fun main(args: Array<String>) = ScriptDownloadAvatars().main(args)
 
@@ -17,6 +20,7 @@ class ScriptDownloadAvatars : CliktCommand(
 ) {
     // Top level options
     private val topLevelOptions by TopLevelOptions()
+    private val timeOptionsParser by TimeOptions()
 
     // Auth
     private val token by option("--token", "-t",
@@ -32,6 +36,8 @@ class ScriptDownloadAvatars : CliktCommand(
             .default(File("avatars"))
     private val useDisplayname by option("--displayname", "-dn",
             help = "Use the display name instead of username").flag()
+    private val includeDate by option("--add-date", "-ad",
+            help = "Adds the date in format ' - yy\\.mm\\.dd' to the folder").flag()
 
     private val includeBots by option("--include-bots", "-ib",
             help = "Download the avatar images of bots").flag()
@@ -41,32 +47,41 @@ class ScriptDownloadAvatars : CliktCommand(
     override fun run() {
         // Fetch additional options
         topLevelOptions.run()
+        val timeOptions = timeOptionsParser.options()
 
         // Get users
         val users = WebApi(token).getUsers().entries.filter { mapEntry ->
             if (!includeDeleted && mapEntry.value.deleted) {
                 false
             } else !(!includeBots && mapEntry.value.isBot)
-        }.sortedBy { it.value.username() }
+        }.sortedBy { getName(useDisplayname, it.value) }
 
-        // Setup
-        val outDir = output.toPath()
+        // Setup folder
+        val outDir = if (includeDate) {
+            val timeNow = Instant.now().atZone(timeOptions.outTz)
+            val folderTimeFormatter = DateTimeFormatter.ofPattern("yy.MM.dd")
+            val extraStr = " - " + folderTimeFormatter.format(timeNow)
+            output.toPath().resolveSibling(output.name + extraStr)
+        } else {
+            output.toPath()
+        }
         ensureFolderExists(outDir)
-        val http = Http()
 
         Log.high("Downloading avatars")
+        val http = Http()
         users.forEach { mapEntry ->
             val url = mapEntry.value.profile.getLargestImage()
-            val name = if (useDisplayname) {
-                mapEntry.value.displayname()
-            } else {
-                mapEntry.value.username()
-            }
-
+            val name = getName(useDisplayname, mapEntry.value)
             val saveLoc = outDir.resolve(name + guessImageExtFromURL(url))
 
             http.downloadFile(url, saveLoc, ignoreIfExists = true)
         }
         Log.high("Avatars downloaded")
+    }
+
+    private fun getName(useDisplayname: Boolean, user: User) = if (useDisplayname) {
+        user.displayname()
+    } else {
+        user.username()
     }
 }
