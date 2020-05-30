3a10da4ac33b48fe7ae8e03f0acc2c9ec49b020d
Holly Roberts <5519754+hollyroberts@users.noreply.github.com>
2020-05-30 17:10:17 +0100

    Make HttpUtils singletons

--- src/main/kotlin/network/http/HttpUtilsBasic.kt
+++ src/main/kotlin/network/http/HttpUtilsBasic.kt
@@ -2,5 +2,7 @@ package network.http
 
 import okhttp3.OkHttpClient
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class HttpUtilsBasic @Inject constructor(): HttpUtils(OkHttpClient())
--- src/main/kotlin/network/http/HttpUtilsToken.kt
+++ src/main/kotlin/network/http/HttpUtilsToken.kt
@@ -2,7 +2,9 @@ package network.http
 
 import okhttp3.OkHttpClient
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class HttpUtilsToken @Inject constructor(
         tokenClient: OkHttpClient
 ): HttpUtils(tokenClient)
