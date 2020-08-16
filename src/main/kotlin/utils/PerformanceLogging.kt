b54ee2d92c925f12c649b5c1a02df3f1e2dfc547
Holly Roberts <5519754+hollyroberts@users.noreply.github.com>
2020-08-16 22:47:28 +0100

    Add in basic logging of memory usage

---
+++ src/main/kotlin/utils/PerformanceLogging.kt
@@ -0,0 +1,15 @@
+package utils
+
+import org.apache.logging.log4j.kotlin.Logging
+
+object PerformanceLogging : Logging {
+    private val runtime by lazy { Runtime.getRuntime() }
+
+    fun outputMemoryUsage() {
+        val freeMemory = runtime.freeMemory()
+        val totalMemory = runtime.totalMemory()
+        val usedMemory = totalMemory - freeMemory
+
+        logger.info { "Used memory: ${formatSize(usedMemory, 4)}, Total memory: ${formatSize(totalMemory, 4)}" }
+    }
+}
