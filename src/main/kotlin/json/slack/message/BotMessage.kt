d67dc647ab5c21fb4fd195e08fe2ee301022fd5b
Holly Roberts <5519754+hollyroberts@users.noreply.github.com>
2020-09-19 17:43:43 +0100

    Make bot_message username nullable, add nullable bot_id

--- src/main/kotlin/json/slack/message/BotMessage.kt
+++ src/main/kotlin/json/slack/message/BotMessage.kt
@@ -1,9 +1,12 @@
 package json.slack.message
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 class BotMessage(
-        val username: String,
+        val username: String?,
+        @Json(name = "bot_id")
+        val botId: String?,
         val text: String
 ) : BaseMessage()
