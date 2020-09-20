acb8f5a37e8466042f7923e55e37659450000ac9
Holly Roberts <5519754+hollyroberts@users.noreply.github.com>
2020-09-20 16:47:14 +0100

    Optimise reading of type/subtype

--- src/main/kotlin/json/slack/message/SlackMessageAdapter.kt
+++ src/main/kotlin/json/slack/message/SlackMessageAdapter.kt
@@ -33,26 +33,31 @@ class SlackMessageAdapter @Inject constructor(
         var type: String? = null
         var subtypeStr: String? = null
 
-        // TODO Use extension functions to consume object when kotlin supports non-local breaks
+        // We don't use JsonReader.selectName() for this because it's less efficient
+        // The internal Okio select implementation operates on bytes rather than UTF-8
+        // This means that if it doesn't match (ie. -1) it needs to parse the name anyway and then do a more standard comparison
+        // Even worse the comparison is done by looping over each options value and checking for equality (rather than say a map lookup)
+        // In the majority of cases messages don't have subtypes, so we need to peek the entire object and all its keys
+        // The above means that this is actually a lot less efficient that just reading the name and doing a switch lookup
         peekedReader.beginObject()
         while (peekedReader.hasNext()) {
-            when (peekedReader.selectName(keys)) {
-                // It might be worth figuring out if we can make this use Options to improve performance?
+            when (peekedReader.nextName()) {
+                // It might be worth figuring out if we can use Options for type/subtype to improve performance?
                 // The issue is mapping the various types/subtypes to numbers and retaining the switch lookup performance
                 // For the type we could check if it's 1/0
                 // For the subtype we'd have to figure out the available subtypes defined, and then use that for our map
-
-                // Performance: Could we only do the null checks if not -1?
-                0 -> type = peekedReader.nextString()
-                1 -> subtypeStr = peekedReader.nextString()
-                -1 -> peekedReader.skipValue()
-            }
-
-            if (type != null && subtypeStr != null) {
-                break
+                "type" -> {
+                    type = peekedReader.nextString()
+                    if (type != null && subtypeStr != null) break
+                }
+                "subtype" -> {
+                    subtypeStr = peekedReader.nextString()
+                    if (type != null && subtypeStr != null) break
+                }
+                else -> peekedReader.skipValue()
             }
         }
-        // Don't call endObject on the peeked reader
+        // We don't need to call endObject as the peeked reader will be discarded
 
         // Parse message into actual type
         if (type != "message") {
