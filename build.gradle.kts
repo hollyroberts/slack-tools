33591c9710f0714e3c7a05110a5e8ed3fb3f2ea5
Holly Roberts <5519754+hollyroberts@users.noreply.github.com>
2022-06-20 19:10:01 +0100

    Update kotlin to 1.7.0 and update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    kotlin("jvm") version "1.6.21"
-    kotlin("kapt") version "1.6.21"
-    id("com.google.devtools.ksp") version "1.6.21-1.0.5"
+    kotlin("jvm") version "1.7.0"
+    kotlin("kapt") version "1.7.0"
+    id("com.google.devtools.ksp") version "1.7.0-1.0.6"
     id("com.github.ben-manes.versions") version "0.42.0"
 }
 
@@ -36,13 +36,13 @@ dependencies {
     val log4j2Version = "2.17.2"
     val daggerVersion = "2.42"
     val moshiVersion = "1.13.0"
-    val okhttpVersion = "4.9.3"
+    val okhttpVersion = "4.10.0"
     val retrofitVersion = "2.9.0"
     val jmhVersion = "1.35"
 
     // Dependencies
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.21") // Specify reflect library explicitly to stop duplicate classpath build warnings
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.0") // Specify reflect library explicitly to stop duplicate classpath build warnings
 
     implementation("org.apache.logging.log4j:log4j-api-kotlin:1.1.0")
     implementation("org.apache.logging.log4j:log4j-api:$log4j2Version")
@@ -60,17 +60,17 @@ dependencies {
     implementation("com.squareup.moshi:moshi-adapters:$moshiVersion")
     ksp("com.squareup.moshi:moshi-kotlin-codegen:$moshiVersion")
 
-    implementation("com.github.ajalt.clikt:clikt:3.4.2")
+    implementation("com.github.ajalt.clikt:clikt:3.5.0")
 
     // Test dependencies
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
-    testImplementation("org.assertj:assertj-core:3.22.0")
+    testImplementation("org.assertj:assertj-core:3.23.1")
     testImplementation("io.mockk:mockk:1.12.4")
 
     kaptTest("com.google.dagger:dagger-compiler:$daggerVersion")
 
     testImplementation("com.squareup.okhttp3:mockwebserver:$okhttpVersion")
-    testImplementation("io.github.classgraph:classgraph:4.8.146")
+    testImplementation("io.github.classgraph:classgraph:4.8.147")
 
     // Bench dependencies
     benchImplementation("com.google.jimfs:jimfs:1.2")
